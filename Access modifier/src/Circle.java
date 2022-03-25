ab421f0e9b939f004cb6b1e2c4f101e1338f9c6d
dunghojava <dungho21397@gmail.com>
2022-03-25 11:32:28 +0700

    Access modifier

---
+++ Access modifier/src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
