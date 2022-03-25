e9adf750631fae4f01cca9e911a6b0be9b87af2a
dunghojava <dungho21397@gmail.com>
2022-03-25 11:43:34 +0700

    Xay dung lop chi ghi trong Java

---
+++ Class Students/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Dung");
+        student.setClasses("C03");
+        System.out.println(student.getClasses());
+    }
+}
